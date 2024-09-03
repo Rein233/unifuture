d06e6d37ca4a7a87defc21debb84dfe66b7403e1
Rein233 <2694313533@qq.com>
2024-09-03 10:54:10 +0800

    2024/9/3

---
+++ src/test/java/com/future/ssm/config/CodeGenerator.java
@@ -0,0 +1,18 @@
+package com.future.ssm.config;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+
+import java.util.Scanner;
+
+public class CodeGenerator {
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        String projectPath = System.getProperty("user.dir");
+        // 子模块
+        String model = ""
+    }
+
+}
